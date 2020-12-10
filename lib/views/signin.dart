80b0bf194f4655a9a820ad2160a862ef40f7187d
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-10 15:48:07 +0900

    signin.dartの作成

---
+++ lib/views/signin.dart
@@ -0,0 +1,14 @@
+import 'package:chat_app/Widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
