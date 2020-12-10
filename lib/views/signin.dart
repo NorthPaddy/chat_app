7b065dc4222a20ca8f4423a88fd3095f07a4ef0a
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-10 15:53:48 +0900

    TextField&TextStyle Widgetの作成

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -9,6 +9,23 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            TextField(
+              style: simpleTextFieldStyle(),
+              decoration: textFieldInputDecoration('email'),
+            ),
+            TextField(
+              style: simpleTextFieldStyle(),
+              decoration: textFieldInputDecoration('password'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
