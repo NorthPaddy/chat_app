bf14dd6fd7c68d4ea3225595069b22b69faf8ff9
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-12 17:27:24 +0900

    signUpのUIとコントローラーの記述

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/Widgets/widget.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -6,8 +7,105 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 50,
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: userNameTextEditingController,
+                  style: simpleTextStyle(),
+                  decoration: textFieldInputDecoration('名前'),
+                ),
+                TextField(
+                  controller: emailTextEditingController,
+                  style: simpleTextStyle(),
+                  decoration: textFieldInputDecoration('メールアドレス'),
+                ),
+                TextField(
+                  controller: passwordTextEditingController,
+                  style: simpleTextStyle(),
+                  decoration: textFieldInputDecoration('パスワード'),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Text(
+                      'パスワードを忘れましたか?',
+                      style: simpleTextStyle(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC)
+                      ]),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Text(
+                    'サインアップ',
+                    style: mediumTextStyle(),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Text(
+                    'Googleでサインアップする',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "アカウントがありますか？",
+                      style: mediumTextStyle(),
+                    ),
+                    Text(
+                      'ログイン！',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        decoration: TextDecoration.underline,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 50),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
