d9557c70b57df3fe0a51f5a672c52700696cb664
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-14 19:32:11 +0900

    認証に関する関数を作成

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,1 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:chat_app/models/user.dart';
+import 'package:flutter/material.dart';
 
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Users _userFromFirebaseUser(User user) {
+    return user != null ? Users(userId: user.uid) : null;
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future resetPass(String email) async {
+    try {
+      return await _auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
