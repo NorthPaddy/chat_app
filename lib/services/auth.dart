1029d483f9c258972269fb832ac1804435cb9a9c
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-15 21:00:38 +0900

    materialPackage削除

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:chat_app/models/user.dart';
-import 'package:flutter/material.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
