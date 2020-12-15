a1ceec21a3a361d9a26d942071e437710744dbfe
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-16 01:35:24 +0900

    ChatRoomからのログアウトの遷移

--- lib/views/chatRoomScreen.dart
+++ lib/views/chatRoomScreen.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/services/auth.dart';
+import 'package:chat_app/views/signin.dart';
 import 'package:flutter/material.dart';
 
 class ChatRoom extends StatefulWidget {
@@ -6,8 +8,30 @@ class ChatRoom extends StatefulWidget {
 }
 
 class _ChatRoomState extends State<ChatRoom> {
+  AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/images/logo.png",
+          height: 50,
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              authService.signOut();
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => SignIn()));
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.exit_to_app),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
