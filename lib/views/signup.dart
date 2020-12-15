b658624a1ccce2eb35e6b55066088f07892af0b5
NorthPaddy <believe.you.trust.you@gmail.com>
2020-12-15 17:11:21 +0900

    signUp時のバリデーションの設定

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -7,105 +7,152 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  bool isLoading = false;
+
+  final formKey = GlobalKey<FormState>();
   TextEditingController userNameTextEditingController = TextEditingController();
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+
+  signMeUp() {
+    if (formKey.currentState.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height - 50,
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(
-                  controller: userNameTextEditingController,
-                  style: simpleTextStyle(),
-                  decoration: textFieldInputDecoration('名前'),
-                ),
-                TextField(
-                  controller: emailTextEditingController,
-                  style: simpleTextStyle(),
-                  decoration: textFieldInputDecoration('メールアドレス'),
-                ),
-                TextField(
-                  controller: passwordTextEditingController,
-                  style: simpleTextStyle(),
-                  decoration: textFieldInputDecoration('パスワード'),
-                ),
-                SizedBox(height: 8),
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Text(
-                      'パスワードを忘れましたか?',
-                      style: simpleTextStyle(),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 8),
-                Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                        const Color(0xff007EF4),
-                        const Color(0xff2A75BC)
-                      ]),
-                      borderRadius: BorderRadius.circular(30)),
-                  child: Text(
-                    'サインアップ',
-                    style: mediumTextStyle(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30)),
-                  child: Text(
-                    'Googleでサインアップする',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 17,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "アカウントがありますか？",
-                      style: mediumTextStyle(),
-                    ),
-                    Text(
-                      'ログイン！',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        decoration: TextDecoration.underline,
+      body: isLoading
+          ? Container(
+              child: Center(child: CircularProgressIndicator()),
+            )
+          : SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height - 50,
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Form(
+                        key: formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              validator: (val) {
+                                return val.isEmpty || val.length < 3
+                                    ? "名前は３文字以上で入力して下さい"
+                                    : null;
+                              },
+                              controller: userNameTextEditingController,
+                              style: simpleTextStyle(),
+                              decoration: textFieldInputDecoration('名前'),
+                            ),
+                            TextFormField(
+                              validator: (val) {
+                                return RegExp(
+                                            r"^[a-zA-A0-9,!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                        .hasMatch(val)
+                                    ? null
+                                    : "正しいメールアドレスを入力して下さい。";
+                              },
+                              controller: emailTextEditingController,
+                              style: simpleTextStyle(),
+                              decoration: textFieldInputDecoration('メールアドレス'),
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              validator: (val) {
+                                return val.length > 6
+                                    ? null
+                                    : "パスワードは6文字以上で入力して下さい";
+                              },
+                              controller: passwordTextEditingController,
+                              style: simpleTextStyle(),
+                              decoration: textFieldInputDecoration('パスワード'),
+                            ),
+                          ],
+                        ),
                       ),
-                    )
-                  ],
+                      SizedBox(height: 8),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                          child: Text(
+                            'パスワードを忘れましたか?',
+                            style: simpleTextStyle(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        onTap: () {
+                          //TODO
+                          signMeUp();
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(colors: [
+                                const Color(0xff007EF4),
+                                const Color(0xff2A75BC)
+                              ]),
+                              borderRadius: BorderRadius.circular(30)),
+                          child: Text(
+                            'サインアップ',
+                            style: mediumTextStyle(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30)),
+                        child: Text(
+                          'Googleでサインアップする',
+                          style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "アカウントはありますか？",
+                            style: mediumTextStyle(),
+                          ),
+                          Text(
+                            'ログインはこちら',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              decoration: TextDecoration.underline,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 50),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 50),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
